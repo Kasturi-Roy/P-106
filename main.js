7687f8466d3227d3e707496ff2008d63e7760990
Kasturi-Roy <88187759+Kasturi-Roy@users.noreply.github.com>
2021-09-13 14:19:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-//https://teachablemachine.withgoogle.com/models/eqdGMOfUn/
+//https://teachablemachine.withgoogle.com/models/eqdGMOfUn/model.json
+prediction1 = "";
+prediction2 = "";
+
+Webcam.set({
+    height: 300,
+    width:350,
+    image_format: "jpg",
+    jpg_quality: 90
+
+})
+
+camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img id='captured_image' src='"+data_uri+"'/>";
+    });
+}
+
+console.log("ml5 version", ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/eqdGMOfUn/model.json", modelloaded );
+
+function modelloaded() { 
+    console.log("model loaded");
+}
